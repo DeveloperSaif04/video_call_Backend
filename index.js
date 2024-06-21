20c3c9bf1a356557343ef28828ab91562045ebf5
DeveloperSaif04 <saif.coder04@gmail.com>
2024-06-21 16:30:29 +0530

    cors add

--- index.js
+++ index.js
@@ -1,8 +1,62 @@
-const { Server } = require("socket.io");
+// const { Server } = require("socket.io");
 
-const io = new Server(8000, {
-  cors: true,
+// const io = new Server(8000, {
+//   cors: true,
+// });
+
+
+// const emailToSocketIdMap = new Map();
+// const socketidToEmailMap = new Map();
+
+// io.on("connection", (socket) => {
+//   console.log(`Socket Connected`, socket.id);
+//   socket.on("room:join", (data) => {
+//     const { email, room } = data;
+//     emailToSocketIdMap.set(email, socket.id);
+//     socketidToEmailMap.set(socket.id, email);
+//     io.to(room).emit("user:joined", { email, id: socket.id });
+//     socket.join(room);
+//     io.to(socket.id).emit("room:join", data);
+//   });
+
+//   socket.on("user:call", ({ to, offer }) => {
+//     io.to(to).emit("incomming:call", { from: socket.id, offer });
+//   });
+
+//   socket.on("call:accepted", ({ to, ans }) => {
+//     io.to(to).emit("call:accepted", { from: socket.id, ans });
+//   });
+
+//   socket.on("peer:nego:needed", ({ to, offer }) => {
+//     console.log("peer:nego:needed", offer);
+//     io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+//   });
+
+//   socket.on("peer:nego:done", ({ to, ans }) => {
+//     console.log("peer:nego:done", ans);
+//     io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+//   });
+// });
+
+
+const { Server } = require("socket.io");
+const cors = require('cors');
+const express = require('express');
+
+const app = express();
+const server = require('http').createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "https://videocallfrontend.netlify.app", // specify the frontend URL
+    methods: ["GET", "POST"]
+  }
 });
+
+// Apply CORS middleware to the Express app
+app.use(cors({
+  origin: "https://videocallfrontend.netlify.app" // specify the frontend URL
+}));
 
 const emailToSocketIdMap = new Map();
 const socketidToEmailMap = new Map();
@@ -36,3 +90,9 @@ io.on("connection", (socket) => {
     io.to(to).emit("peer:nego:final", { from: socket.id, ans });
   });
 });
+
+const PORT = 8000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
